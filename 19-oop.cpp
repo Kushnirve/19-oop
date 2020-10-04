d27b6eeea9b7a79dc5002821f6889cc54fecbbab
hairpin <60984178+Kushnirve@users.noreply.github.com>
2020-10-04 18:23:34 +0300

    added code

---
+++ 19-oop.cpp
@@ -0,0 +1,13 @@
+﻿#include "Student.h"
+
+int main(){
+	Student student1;
+	student1.setName("Ivan Abram");
+	student1.setYear(1998);
+	student1.setGroup("Rpi19");
+	student1.setInst("Shevchenko");
+	student1.show();
+
+	Student student2("Fedor Petrov", 1999, "Rpo07", "Kozubinski");
+	student2.show();
+}
