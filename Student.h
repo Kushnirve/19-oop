1b43284497253fa0aacc4a89d5f1077be3b49b5e
hairpin <60984178+Kushnirve@users.noreply.github.com>
2020-10-15 13:48:49 +0300

    rework

--- Student.h
+++ Student.h
@@ -5,50 +5,22 @@ using namespace std;
 class Student {
 	char* name;
 	int year;
-	char* group;	
-	char* inst;
+	char* group;
+	char* institution;
 
 public:
 	Student();
-	Student(const char* newName, int newYear, const char* newGroup, const char* newInst);
-
-	void setName(const char* newName){
-		delete[] name;
-		createRecord(newName, 1);
-	}
-
-	void setYear(int newYear) {
-		if (newYear < 1950 or newYear > 2010) newYear = 0;
-		year = newYear;
-	}
-
-	void setGroup(const char* newGroup) {
-		delete[] group;
-		createRecord(newGroup, 2);
-	}
-
-	void setInst(const char* newInst) {
-		delete[] inst;
-		createRecord(newInst, 3);
-	}
-
-	const char* getName() {
-		return name;
-	}
-
-	const int getYear() {
-		return year;
-	}
-
-	const char* getGroup() {
-		return group;
-	}
-
-	const char* getInst() {
-		return inst;
-	}
-
-	void show();	
+	Student(const char* newName, int newYear, const char* newGroup, const char* newInstitution);
+	Student(const Student& obj);
+	const char* getName() { return name; }
+	const int getYear() { return year; }
+	const char* getGroup() { return group; }
+	const char* getInstitution() { return institution; }
+	void setName(const char* newName) { delete[] name; createRecord(newName, 1); }
+	void setYear(int newYear) { (newYear < 1950 or newYear > 2010) ? year = 0 : year = newYear; }
+	void setGroup(const char* newGroup) { delete[] group; createRecord(newGroup, 2); }
+	void setInstitution(const char* newInstitution) { delete[] institution;	createRecord(newInstitution, 3); }
+	void print();
 	void createRecord(const char* data, int sel);
 	~Student();
 };
