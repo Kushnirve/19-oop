b624f4739d35b0f3d0b3eb0f017188978359cfa2
hairpin <60984178+Kushnirve@users.noreply.github.com>
2020-10-15 13:49:23 +0300

    rework

--- 19-oop.cpp
+++ 19-oop.cpp
@@ -1,13 +1,19 @@
 ﻿#include "Student.h"
 
-int main(){
+int main() {
 	Student student1;
-	student1.setName("Ivan Abram");
-	student1.setYear(1998);
-	student1.setGroup("Rpi19");
-	student1.setInst("Shevchenko");
-	student1.show();
+	student1.setName("Ivanov Vasiliy");
+	student1.setYear(2003);
+	student1.setGroup("Metallica");
+	student1.setInstitution("KPI");
+	student1.print();
 
-	Student student2("Fedor Petrov", 1999, "Rpo07", "Kozubinski");
-	student2.show();
+	Student student2("Petrov Fedya", 1982, "Risk group", "IT Step Academy");
+	student2.print();
+
+	Student	student3 = student1;
+	student3.print();
+
+	Student	student4(student2);
+	student4.print();
 }
